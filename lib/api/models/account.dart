MaroMetelski/budgee-app

Basic login and entry pages
Create API class to access remote API. Implement basics:
/login GET
/account GET
/entry GET

Add 'working' login and 'entry' page.

lib/api/models/account.dart
@@ -1,0 +1,25 @@
+class Account {
+  final String id;
+  final String name;
+  final String type;
+  final String extraType;
+  final String description;
+
+  const Account({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.extraType,
+    required this.description,
+  });
+
+  factory Account.fromJson(Map<String, dynamic> json) {
+    return Account(
+      id: json["id"],
+      name: json["name"],
+      type: json["type"],
+      extraType: json["extraType"],
+      description: json["description"],
+    );
+  }
+}

lib/api/models/entry.dart
@@ -1,0 +1,45 @@
+class Entry {
+  final String id;
+  final String amount;
+  final String who;
+  final String date;
+  final String cr;
+  final String dr;
+  final String description;
+  final List<dynamic> tags;
+
+  const Entry({
+    required this.id,
+    required this.amount,
+    required this.who,
+    required this.date,
+    required this.cr,
+    required this.dr,
+    required this.description,
+    required this.tags,
+  });
+
+  factory Entry.fromJson(Map<String, dynamic> json) {
+    return Entry(
+      id: json["id"],
+      amount: json["amount"],
+      who: json["who"],
+      date: json["when"],
+      cr: json["credit_account"],
+      dr: json["debit_account"],
+      description: json["description"],
+      tags: json["tags"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'amount': amount,
+    'who': who,
+    'date': date,
+    'credit_account': cr,
+    'debit_account': dr,
+    'description': description,
+    'tags': tags,
+  };
+}

lib/login.dart
@@ -1,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'api/api.dart';
+import 'entry.dart';
+
+
+class LoginPage extends StatefulWidget {
+
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _loggedIn = false;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    void onButton(name, password) async {
+      _loggedIn = await Provider.of<ApiService>(context, listen: false).getLogin(name, password);
+     if (mounted && _loggedIn) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const EntryPage()),
+          (Route<dynamic> route) => false,
+        );
+      }
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "username",
+                ),
+                controller: _usernameController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "password",
+                ),
+                controller: _passwordController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  onButton(_usernameController.text, _passwordController.text);
+                },
+                child: const Text("log in"),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

MaroMetelski/budgie-app

lib/api/models/account.dart
@@ -1,0 +1,25 @@
+class Account {
+  final String id;
+  final String name;
+  final String type;
+  final String extraType;
+  final String description;
+
+  const Account({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.extraType,
+    required this.description,
+  });
+
+  factory Account.fromJson(Map<String, dynamic> json) {
+    return Account(
+      id: json["id"],
+      name: json["name"],
+      type: json["type"],
+      extraType: json["extra_type"],
+      description: json["description"],
+    );
+  }
+}

lib/api/models/entry.dart
@@ -1,0 +1,45 @@
+class Entry {
+  final String id;
+  final String amount;
+  final String who;
+  final String date;
+  final String cr;
+  final String dr;
+  final String description;
+  final List<String> tags;
+
+  const Entry({
+    required this.id,
+    required this.amount,
+    required this.who,
+    required this.date,
+    required this.cr,
+    required this.dr,
+    required this.description,
+    required this.tags,
+  });
+
+  factory Entry.fromJson(Map<String, dynamic> json) {
+    return Entry(
+      id: json["id"],
+      amount: json["amount"],
+      who: json["who"],
+      date: json["when"],
+      cr: json["credit_account"],
+      dr: json["debit_account"],
+      description: json["description"],
+      tags: json["tags"].isNotEmpty ? List<String>.from(json["tags"]) : <String>[],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'amount': amount,
+    'who': who,
+    'date': date,
+    'credit_account': cr,
+    'debit_account': dr,
+    'description': description,
+    'tags': tags,
+  };
+}

lib/login/ui/login.dart
@@ -1,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../api/api.dart';
+import '../../transactions/ui/transaction.dart';
+
+class LoginPage extends StatefulWidget {
+
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _loggedIn = false;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    void onButton(name, password) async {
+      _loggedIn = await Provider.of<ApiService>(context, listen: false).getLogin(name, password);
+     if (mounted && _loggedIn) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const TransactionList()),
+          (Route<dynamic> route) => false,
+        );
+      }
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "username",
+                ),
+                controller: _usernameController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "password",
+                ),
+                controller: _passwordController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  onButton(_usernameController.text, _passwordController.text);
+                },
+                child: const Text("log in"),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}